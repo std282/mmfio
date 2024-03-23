80be62b69f78316ab29a9ed7d7c8c9c0b7643403
Alexander Shokhin <std282@icloud.com>
2024-03-23 21:50:22 +0300

    fixed bug in mmfseterror
    I used snprintf instead of vsnprintf. Bruh.

--- mmfio.h
+++ mmfio.h
@@ -64,7 +64,7 @@ static void mmfseterror(const char* fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
-  snprintf(mmferrorbuffer, sizeof(mmferrorbuffer), fmt, args);
+  vsnprintf(mmferrorbuffer, sizeof(mmferrorbuffer), fmt, args);
   va_end(args);
 }
 
